58c198e221148977029b1291479b9660071a0065
Szymon Sasin <szymon@sasin.eu>
2016-11-29 22:32:12 +0000

    Added more tests for maybe

--- Tests/TheLizzards.Maybe.Tests/MaybeTests.cs
+++ Tests/TheLizzards.Maybe.Tests/MaybeTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using FluentAssertions;
 using TheLizzards.Maybe;
 using Xunit;
 
@@ -13,6 +14,57 @@ namespace TheLizzards.Maybe.Tests
 			Maybe<string> possibleMaybe = null;
 
 			Assert.NotNull(possibleMaybe);
+		}
+
+		[Theory]
+		[InlineData(5, 3, 1)]
+		[InlineData(5, 8, -1)]
+		[InlineData(5, 5, 0)]
+		public void ComparisionWorksForPayload(int value, int compareTo, int result)
+		{
+			var maybe = Maybe.From(value);
+
+			var comparisionValue = maybe.CompareTo(compareTo);
+
+			comparisionValue.Should().Be(result);
+		}
+
+		[Theory]
+		[InlineData(5, 3, 1)]
+		[InlineData(5, 8, -1)]
+		[InlineData(5, 5, 0)]
+		public void ComparisionWorksForMaybes(int value, int compareTo, int result)
+		{
+			var maybe = Maybe.From(value);
+			var other = Maybe.From(compareTo);
+
+			var comparisionValue = maybe.CompareTo(other);
+
+			comparisionValue.Should().Be(result);
+		}
+
+		[Theory]
+		[InlineData(5, 5, true)]
+		[InlineData(5, 8, false)]
+		public void EqualityWorksForPayload(int value, int compareTo, bool result)
+		{
+			var maybe = Maybe.From(value);
+
+			var comparisionValue = maybe.Equals(compareTo);
+
+			comparisionValue.Should().Be(result);
+		}
+
+		[Theory]
+		[InlineData(5, 5, true)]
+		[InlineData(5, 8, false)]
+		public void EqualityWorksForMaybes(int value, int compareTo, bool result)
+		{
+			var maybe = Maybe.From(value);
+
+			var comparisionValue = maybe.Equals(compareTo);
+
+			comparisionValue.Should().Be(result);
 		}
 	}
 }
