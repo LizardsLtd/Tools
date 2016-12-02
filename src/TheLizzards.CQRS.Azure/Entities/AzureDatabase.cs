d825d42633e280d2f0226ea6aebbf39e065d0896
Szymon Sasin <szymon@sasin.eu>
2016-12-02 15:56:09 +0000

    Added new functionality

--- src/TheLizzards.CQRS.Azure/Entities/AzureDatabase.cs
+++ src/TheLizzards.CQRS.Azure/Entities/AzureDatabase.cs
@@ -1,18 +1,45 @@
 using System;
 using System.Linq;
+using Microsoft.Azure.Documents.Client;
 
 namespace TheLizzards.CQRS.Azure.Entities
 {
-  public sealed class AzureDatabase
-  {
-    public AzureDatabase(string name, IEnumerable<string> collections)
-    {
-      this.Name = name;
-      this.Collections = collections.ToArray();
-    }
+	public sealed class AzureDatabase
+	{
+		public AzureDatabase(string databaseId, IEnumerable<string> collections)
+		{
+			this.DatabaseId = databaseId;
+			this.Collections = collections.ToArray();
+		}
+   
+		public string DatabaseId { get; }
     
-    public string Name { get; }
-    
-    public string[] Collections{ get; }
-  }
+		public string[] Collections{ get; }
+	 
+		internal void CreateDatabaseWithCollection(DatabaseClient client)
+		{
+			var database = this.CreateDatabase();
+		}
+		public void CreateDatabases()
+			=> this.databases
+				.Select(this.CreateDatabase)
+				.Where(DoesDatabaseHasToBeCreated)
+				.ToList()
+				.ForEach(async db => await this.client.CreateDatabaseAsync(db));
+
+		private bool DoesDatabaseHasToBeCreated(Database database)
+			=> !DoesDatabseExist(database);
+
+		private bool DoesDatabseExist(Database database)
+			=> client.CreateDatabaseQuery()
+				.Where(db => db.Id == database.Id)
+				.ToArray()
+				.Any();
+
+		private Database CreateDatabase()
+			=> new Database
+			{
+				Id = databaseId,
+			};
+	}
 }
