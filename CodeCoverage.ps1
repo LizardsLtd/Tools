ebe38a8db4f7d648ced56646ccce29cd31c836a4
Szymon Sasin <szymon@sasin.eu>
2016-12-05 14:12:44 +0000

    Updated code coverage calculation

--- CodeCoverage.ps1
+++ CodeCoverage.ps1
@@ -1,12 +1,14 @@
 nuget install OpenCover -version 4.6.519
 
-get-item ".\Tests\*\project.json" |
- foreach { 
-    .\OpenCover.4.6.519\tools\OpenCover.Console.exe `
-        "-target:C:\Program Files\dotnet\dotnet.exe" `
-        "-targetargs:test $_" `
-        -register:user `
-        "-filter:+[*]* -[xunit*]*" `
-        -oldStyle `
-        -output:coverage.$($_.Directory.Name).xml ` 
- }
+$projects = get-item ".\Tests\*\project.json" |
+    foreach { $_.FullName }
+
+$command = $projects -join " " 
+    
+.\OpenCover.4.6.519\tools\OpenCover.Console.exe `
+    "-target:C:\Program Files\dotnet\dotnet.exe" `
+    "-targetargs:test $($command)" `
+    -register:user `
+    "-filter:+[*]* -[xunit*]*" `
+    -oldStyle `
+    -output:coverage.xml
