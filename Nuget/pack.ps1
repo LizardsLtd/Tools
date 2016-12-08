c6c5f83540f30d98cb4ba4c9ec5d87b3799159e9
Szymon Sasin <szymon@sasin.eu>
2016-12-08 12:40:45 +0000

    remove not used script

--- Nuget/pack.ps1
+++
@@ -1,8 +0,0 @@
-param(
-    [string] $configuration,
-    [string] $outputLocation,
-    [string] $packageVersion
-)
-
-dotnet restore
-Get-ChildItem "src/*/project.json" | % { dotnet pack $_.FullName -c $configuration -o $outputLocation --version-suffix $packageVersion -s}
