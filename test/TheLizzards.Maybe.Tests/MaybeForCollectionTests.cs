2deabcedb099a3825b0805e75ebc997db5a28be2
Szymon Sasin <szymon@sasin.eu>
2016-12-11 23:19:00 +0000

    Added more test for Maybe but still need to improve code coverage

---
+++ test/TheLizzards.Maybe.Tests/MaybeForCollectionTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TheLizzards.Maybe;
+using Xunit;
+
+namespace TheLizzards.Common.Tests
+{
+	public sealed class MaybeForCollectionTests
+	{
+		private readonly List<int> exampleCollection;
+
+		public MaybeForCollectionTests()
+		{
+			this.exampleCollection = new List<int>
+			{
+				1,
+				2,
+				3,
+				4,
+				5,
+			};
+		}
+
+		[Fact]
+		public void CastToMaybeCollectionWorks()
+		{
+			var collection = this.exampleCollection.ToMaybeList();
+
+			collection.All(x => x.IsSome);
+		}
+	}
+}
