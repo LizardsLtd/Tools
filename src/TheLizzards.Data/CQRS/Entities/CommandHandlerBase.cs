7ac3b4bf962444352e595e1a9e5c2799122cf4df
Szymon Sasin <szymon@sasin.eu>
2016-12-13 15:32:40 +0000

    trying to fix build errors

--- src/TheLizzards.Data/CQRS/Entities/CommandHandlerBase.cs
+++ src/TheLizzards.Data/CQRS/Entities/CommandHandlerBase.cs
@@ -22,7 +22,7 @@ namespace TheLizzards.Data.CQRS.Entities
 			=> await this.Execute((TCommand)command);
 
 		public async Task<IEnumerable<ValidationResult>> Validate(ICommand command)
-			=> this.Validate((TCommand)command);
+			=> await this.Validate((TCommand)command);
 
 		public void Dispose() => Dispose(true);
 
@@ -33,7 +33,7 @@ namespace TheLizzards.Data.CQRS.Entities
 		protected abstract Task Execute(TCommand command);
 
 		protected virtual Task<IEnumerable<ValidationResult>> Validate(TCommand commmand)
-			=> Task.Run(() => new ValidationResult[0]);
+			=> Task.Run<IEnumerable<ValidationResult>(() => new ValidationResult[0]);
 
 		private void Dispose(bool disposing)
 		{
