20ff1688f00729c9994cd06c56a483e21fd3a167
Sasin <Szymon.Sasin@Fiserv.com>
2016-12-19 08:59:22 +0000

    added empty test to chekc if it can fix build server

---
+++ test/TheLizzards.FileSystem.Tests/Test.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Linq;
+using Xunit;
+
+namespace TheLizzards.FileSystem.Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void PassingTest(){
+            Assert.True(true);
+        }
+    }
+}
