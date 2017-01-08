9ee24b0caa4b554eb279eaeb5f88236b7429eee5
Szymon Sasin <szymon@sasin.eu>
2017-01-08 20:48:14 +0000

    Adding search results tests

--- test/TheLizzards.Search.Tests/Entities/SearchResults.cs
+++ test/TheLizzards.Search.Tests/Entities/SearchResults.cs
@@ -4,14 +4,27 @@ using Xunit;
 
 namespace TheLizzards.Search.Tests.Entities
 {
-    public class SearchResults
-    {
-        [Fact]
-        public void CreateEmptyResultsSet()
-        {
+	public class SearchResults
+	{
+		[Fact]
+		public void CreateEmptyResultsSet()
+		{
 			var results = new SearchResults<SearchResultsPayload>();
 
 			Assert.False(results.HasResults);
-        }
-    }
+		}
+
+		[Fact]
+		public void CreateNonEmptyResults()
+		{
+			var results = new SearchResults<SearchResultsPayload>(
+				new []
+				{
+					new SearchResultsPayload(),
+					new SearchResultsPayload(),
+				});
+
+			Assert.True(results.HasResults);
+		}
+	}
 }
