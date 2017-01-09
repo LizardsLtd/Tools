8247315533eeec1a20f75a76699317a40592540b
Szymon Sasin <szymon@sasin.eu>
2017-01-09 20:29:59 +0000

    Added service extension

--- src/TheLizzards.Localisation/Services/WithoutAddressServiceExtension.cs
+++ src/TheLizzards.Localisation/Services/WithoutAddressServiceExtension.cs
@@ -1,10 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using TheLizzards.Localisation.Entities;
 
 namespace TheLizzards.Localisation.Services
 {
 	public static class WithoutAddressServiceExtension
 	{
+		public static LocationPoint Convert(
+			this ILocalisationService locationService
+			, string houseNumber
+			, string streetName
+			, string district
+			, string city
+			, string province
+			, string country
+			, string postCode)
+		{
+			var address = new Address(houseNumber, streetName, district, city, province, country, postCode);
+
+			return locationService.Convert(address);
+		}
 	}
 }
