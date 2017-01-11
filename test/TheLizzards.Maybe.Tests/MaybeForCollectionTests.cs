360b2b116134444133cf07be416b3cc93e62d4b5
Szymon Sasin <szymon@sasin.eu>
2017-01-11 20:07:54 +0000

    adding more tests

--- test/TheLizzards.Maybe.Tests/MaybeForCollectionTests.cs
+++ test/TheLizzards.Maybe.Tests/MaybeForCollectionTests.cs
@@ -10,14 +10,7 @@ namespace TheLizzards.Maybe.Tests
 
 		public MaybeForCollectionTests()
 		{
-			this.exampleCollection = new List<int>
-			{
-				1,
-				2,
-				3,
-				4,
-				5,
-			};
+			this.exampleCollection = new List<int> { 1, 2, 3, 4, 5, };
 		}
 
 		[Fact]
@@ -25,7 +18,67 @@ namespace TheLizzards.Maybe.Tests
 		{
 			var collection = this.exampleCollection.ToMaybeList();
 
-            Assert.True(collection.All(x => x.IsSome));
+			Assert.True(collection.All(x => x.IsSome));
 		}
+
+		[Fact]
+		public void SingleOrNothiong()
+		{
+			var collection = new List<int> { 1 };
+
+			var result = collection.SingleOrNothing();
+
+			Assert.True(result.IsSome);
+		}
+
+		[Fact]
+		public void SingleOrNothingWoithQuery()
+		{
+			var result = this
+				.exampleCollection
+				.SingleOrNothing(x => x == 1);
+
+			Assert.True(result.IsSome);
+		}
+
+		//[Fact]
+		//public void SingleOrNothiong()
+		//{
+		//	var collection = new List<int> { 1 };
+
+		//	var result = collection.SingleOrNothing();
+
+		//	Assert.True(result.IsSome);
+		//}
+
+		//[Fact]
+		//public void SingleOrNothingWoithQuery()
+		//{
+		//	var result = this
+		//		.exampleCollection
+		//		.SingleOrNothing(x => x == 1);
+
+		//	Assert.True(result.IsSome);
+		//}
+
+		//[Fact]
+		//public void SingleOrNothiong()
+		//{
+		//	var collection = new List<int> { 1 };
+
+		//	var result = collection.SingleOrNothing();
+
+		//	Assert.True(result.IsSome);
+		//}
+
+		//[Fact]
+		//public void SingleOrNothingWoithQuery()
+		//{
+		//	var result = this
+		//		.exampleCollection
+		//		.SingleOrNothing(x => x == 1);
+
+		//	Assert.True(result.IsSome);
+		//}
 	}
 }
