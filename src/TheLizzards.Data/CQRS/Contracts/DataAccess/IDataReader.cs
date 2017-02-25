9c742bb8c735db2d6630c3743d5daba95fe808d4
Szymon Sasin <szymon@sasin.eu>
2017-02-25 06:19:08 +0000

    changed reader to retun IQueryable

--- src/TheLizzards.Data/CQRS/Contracts/DataAccess/IDataReader.cs
+++ src/TheLizzards.Data/CQRS/Contracts/DataAccess/IDataReader.cs
@@ -1,26 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq.Expressions;
-using System.Threading.Tasks;
-using TheLizzards.Data.CQRS.Entities;
+﻿using System.Linq;
 using TheLizzards.Data.DDD.Contracts;
-using TheLizzards.Maybe;
 
 namespace TheLizzards.Data.CQRS.Contracts.DataAccess
 {
 	public interface IDataReader<T>
 		where T : IAggregateRoot
 	{
-		Task<IEnumerable<T>> GetAll(Expression<Func<T, bool>> predicate);
+		IQueryable<T> Query();
 
-		Task<IEnumerable<T>> GetPage(Expression<Func<T, bool>> predicate, Page page);
+		//Task<IEnumerable<T>> GetAll(Expression<Func<T, bool>> predicate);
 
-		Task<T> First(Expression<Func<T, bool>> predicate);
+		//Task<IEnumerable<T>> GetPage(Expression<Func<T, bool>> predicate, Page page);
 
-		Task<Maybe<T>> FirstOrDefault(Expression<Func<T, bool>> predicate);
+		//Task<T> First(Expression<Func<T, bool>> predicate);
 
-		Task<T> Single(Expression<Func<T, bool>> predicate);
+		//Task<Maybe<T>> FirstOrDefault(Expression<Func<T, bool>> predicate);
 
-		Task<Maybe<T>> SingleOrDefault(Expression<Func<T, bool>> predicate);
+		//Task<T> Single(Expression<Func<T, bool>> predicate);
+
+		//Task<Maybe<T>> SingleOrDefault(Expression<Func<T, bool>> predicate);
 	}
 }
