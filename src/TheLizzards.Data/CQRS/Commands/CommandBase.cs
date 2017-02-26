39d3ef357b632a5fdaffd6324ef0a6091387c113
Szymon Sasin <szymon@sasin.eu>
2017-02-26 00:03:25 +0000

    renamed cqrs conected classes

---
+++ src/TheLizzards.Data/CQRS/Commands/CommandBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using TheLizzards.Data.CQRS.Contracts;
+
+namespace TheLizzards.Data.CQRS.Commands
+{
+	public abstract class CommandBase : ICommand
+	{
+		public CommandBase()
+		{
+			this.CommandId = Guid.NewGuid();
+		}
+
+		public Guid CommandId { get; }
+	}
+}
---
+++ src/TheLizzards.Data/CQRS/Queries/QueryForAll.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using TheLizzards.Data.CQRS.Contracts.DataAccess;
+using TheLizzards.Data.DDD.Contracts;
+
+namespace TheLizzards.Data.CQRS.Queries
+{
+	public abstract class QueryForAll<TPayload> : Query<TPayload, IEnumerable<TPayload>>
+		where TPayload : IAggregateRoot
+	{
+		private readonly Guid id;
+
+		public QueryForAll(
+			IDataContext storageContext
+			, ILoggerFactory loggerfactory
+			, DatabaseParts parts, Guid id)
+				: base(storageContext, loggerfactory, parts)
+		{
+			this.id = id;
+		}
+
+		public override Task<IEnumerable<TPayload>> Execute()
+			=> this.Read().All();
+	}
+}
