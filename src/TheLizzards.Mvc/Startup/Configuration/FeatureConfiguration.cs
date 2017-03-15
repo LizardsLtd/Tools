8d8a68ddb33a098a64a06da7fba154cd2f83ea92
Szymon Sasin <szymon@sasin.eu>
2017-03-15 06:49:53 +0000

    trying to sort out configuration

---
+++ src/TheLizzards.Mvc/Startup/Configuration/FeatureConfiguration.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheLizzards.Mvc.Startup.Configuration
+{
+    class FeatureConfiguration
+    {
+    }
+}
--- src/TheLizzards.Mvc/Startup/RazorConfiguration.cs
+++ src/TheLizzards.Mvc/Startup/RazorConfiguration.cs
@@ -3,9 +3,9 @@ using TheLizzards.Mvc.FeatureSlices;
 
 namespace TheLizzards.Mvc.Startup
 {
-    public static class RazorViewBootstrapper
+    public sealed class RazorConfiguration
     {
-        public static IConfiguration AddFeatureSlice(this IConfiguration startup)
+        public RazorConfiguration AddFeatureSlice(this IConfiguration startup)
             => startup.ConfigureOption<RazorViewEngineOptions>(options
                 => new ViewLocationFormatsUpdater(options)
                     .UpdateViewLocations()
