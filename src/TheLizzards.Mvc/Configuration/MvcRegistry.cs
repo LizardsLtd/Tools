3956d59ad7d4061db550a56c55455850b4d4a6aa
Szymon Sasin <szymon@sasin.eu>
2017-03-19 10:05:46 +0000

    added default

--- src/TheLizzards.Mvc/Configuration/MvcRegistry.cs
+++ src/TheLizzards.Mvc/Configuration/MvcRegistry.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc.Localization;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Localization;
 using TheLizzards.I18N.Data;
+using TheLizzards.Mvc.Configuration.Defaults;
 using TheLizzards.Mvc.Localisation;
 using TheLizzards.Mvc.Navigation;
 
@@ -54,6 +55,14 @@ namespace TheLizzards.Mvc.Configuration
         internal void Execute(IApplicationBuilder app)
         {
             //app.UseMvc(this.routeBuilder.BuildRoutes);
+        }
+
+        public MvcRegistry ApplyDefaults<TDefaults>() where TDefaults : IDefaultMvcConfiguration, new()
+        {
+            var defaults = new TDefaults();
+            defaults.Apply(this);
+
+            return this;
         }
 
         private void CreateMvcOptions(MvcOptions options)
