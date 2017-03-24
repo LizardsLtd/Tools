303828b3dd7d6e1a14e4cbcb0f5609e975619a1f
Szymon Sasin <szymon@sasin.eu>
2017-03-24 18:40:48 +0000

    finixshed name change

--- src/Picums.GeoCoding/Entities/GoogleGeocodingResults.cs
+++ src/Picums.GeoCoding/Entities/GoogleGeocodingResults.cs
@@ -1,71 +1,71 @@
-﻿using System.Globalization;
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Spatial;
+using System.Globalization;
 using System.Linq;
 using System.Xml.Linq;
-using Microsoft.Extensions.Logging;
-using Microsoft.Spatial;
 
 namespace Picums.Localisation.Entities
 {
-	internal sealed class GoogleGeocodingResults
-	{
-		private readonly ILogger logger;
+    public sealed class GoogleGeocodingResults
+    {
+        private readonly ILogger logger;
 
-		public GoogleGeocodingResults(ILogger logger, string xmlDocument)
-		{
-			this.logger = logger;
-			var document = XDocument.Parse(xmlDocument);
+        public GoogleGeocodingResults(ILogger logger, string xmlDocument)
+        {
+            this.logger = logger;
+            var document = XDocument.Parse(xmlDocument);
 
-			this.Status = LoadStatus(document);
+            this.Status = LoadStatus(document);
 
-			if (HasResults)
-			{
-				this.Location = LoadLocation(document);
-			}
-		}
+            if (HasResults)
+            {
+                this.Location = LoadLocation(document);
+            }
+        }
 
-		public bool HasResults => Status == "OK";
+        public bool HasResults => Status == "OK";
 
-		public bool HasNoResults => !HasResults;
+        public bool HasNoResults => !HasResults;
 
-		public string Status { get; }
+        public string Status { get; }
 
-		public GeographyPoint Location { get; }
+        public GeographyPoint Location { get; }
 
-		private string LoadStatus(XDocument document)
-											=> document
-				.Document
-				.Descendants(XName.Get("status"))
-				.First()
-				.Value;
+        private string LoadStatus(XDocument document)
+                                            => document
+                .Document
+                .Descendants(XName.Get("status"))
+                .First()
+                .Value;
 
-		private GeographyPoint LoadLocation(XDocument document)
-		{
-			var location = document
-				.Document
-				.Descendants(XName.Get("geometry"))
-				.Descendants(XName.Get("location"))
-				.First();
+        private GeographyPoint LoadLocation(XDocument document)
+        {
+            var location = document
+                .Document
+                .Descendants(XName.Get("geometry"))
+                .Descendants(XName.Get("location"))
+                .First();
 
-			var latitude = GetProperty(location, "lat");
-			var longitude = GetProperty(location, "lng");
+            var latitude = GetProperty(location, "lat");
+            var longitude = GetProperty(location, "lng");
 
-			return GeographyPoint.Create((double)latitude, (double)longitude);
-		}
+            return GeographyPoint.Create((double)latitude, (double)longitude);
+        }
 
-		private double GetProperty(XElement location, string key)
-		{
-			var latitudeAsString = location
-				.Descendants(XName.Get(key))
-				.First()
-				.Value;
+        private double GetProperty(XElement location, string key)
+        {
+            var latitudeAsString = location
+                .Descendants(XName.Get(key))
+                .First()
+                .Value;
 
-			this.logger.LogDebug($"{key}: {latitudeAsString}");
+            this.logger.LogDebug($"{key}: {latitudeAsString}");
 
-			var value = double.Parse(latitudeAsString, CultureInfo.InvariantCulture);
+            var value = double.Parse(latitudeAsString, CultureInfo.InvariantCulture);
 
-			this.logger.LogDebug($"Parsed value: {value}");
+            this.logger.LogDebug($"Parsed value: {value}");
 
-			return value;
-		}
-	}
+            return value;
+        }
+    }
 }
