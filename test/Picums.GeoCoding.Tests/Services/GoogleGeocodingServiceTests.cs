d77e931222b3961ae0220aec430cf5ce38aad114
Szymon Sasin <szymon@sasin.eu>
2017-03-26 08:44:12 +0100

    remove code testing google services

--- test/Picums.GeoCoding.Tests/Services/GoogleGeocodingServiceTests.cs
+++ test/Picums.GeoCoding.Tests/Services/GoogleGeocodingServiceTests.cs
@@ -1,57 +1,57 @@
-﻿using System.Threading.Tasks;
-using Picums.GeoCoding;
-using Picums.Tests;
-using Xunit;
+﻿//using System.Threading.Tasks;
+//using Picums.GeoCoding;
+//using Picums.Tests;
+//using Xunit;
 
-namespace Picums.Location.Tests.Services
-{
-	public sealed class GoogleGeocodingServiceTests
-	{
-		private readonly GoogleGeocodingService service;
+//namespace Picums.Location.Tests.Services
+//{
+//	public sealed class GoogleGeocodingServiceTests
+//	{
+//		private readonly GoogleGeocodingService service;
 
-		public GoogleGeocodingServiceTests()
-		{
-			this.service = new GoogleGeocodingService(new TestLoggerFactory());
-		}
+//		public GoogleGeocodingServiceTests()
+//		{
+//			this.service = new GoogleGeocodingService(new TestLoggerFactory());
+//		}
 
-		[Fact]
-		public async Task ConnectToService()
-		{
-			var result = await this.service.GeocodeAsync(
-				houseNumber: "10"
-				, streetName: "Downing street"
-				, city: "London"
-				, country: "UK");
+//		[Fact]
+//		public async Task ConnectToService()
+//		{
+//			var result = await this.service.GeocodeAsync(
+//				houseNumber: "10"
+//				, streetName: "Downing street"
+//				, city: "London"
+//				, country: "UK");
 
-			Assert.Equal(51.5033635, result.Value.Latitude);
-			Assert.Equal(-0.1276248, result.Value.Longitude);
-		}
+//			Assert.Equal(51.5033635, result.Value.Latitude);
+//			Assert.Equal(-0.1276248, result.Value.Longitude);
+//		}
 
-		[Fact]
-		public async Task TryToChekcKnownAddress()
-		{
-			var result = await this.service.GeocodeAsync(
-				houseNumber: "5"
-				, streetName: "Kochanowskiego"
-				, district: ""
-				, city: "Zgorzelec"
-				, province: "Dolnoslaskie"
-				, country: "poland"
-				, postCode: "50-900");
+//		[Fact]
+//		public async Task TryToChekcKnownAddress()
+//		{
+//			var result = await this.service.GeocodeAsync(
+//				houseNumber: "5"
+//				, streetName: "Kochanowskiego"
+//				, district: ""
+//				, city: "Zgorzelec"
+//				, province: "Dolnoslaskie"
+//				, country: "poland"
+//				, postCode: "50-900");
 
-			Assert.Equal(51.1287637, result.Value.Latitude);
-			Assert.Equal(15.0137526, result.Value.Longitude);
-		}
+//			Assert.Equal(51.1287637, result.Value.Latitude);
+//			Assert.Equal(15.0137526, result.Value.Longitude);
+//		}
 
-		[Fact]
-		public async Task TryToCheckNonExistingAddress()
-		{
-			var result = await this.service.GeocodeAsync(
-				houseNumber: ""
-				, streetName: ""
-				, country: "");
+//		[Fact]
+//		public async Task TryToCheckNonExistingAddress()
+//		{
+//			var result = await this.service.GeocodeAsync(
+//				houseNumber: ""
+//				, streetName: ""
+//				, country: "");
 
-			Assert.True(result.IsNone);
-		}
-	}
-}
+//			Assert.True(result.IsNone);
+//		}
+//	}
+//}
