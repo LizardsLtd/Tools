2b27a7031f62c46625f0af606ab2dca28f12106f
Szymon Sasin <szymon@sasin.eu>
2017-03-28 21:56:08 +0100

    added statis file default

---
+++ src/Picums.Mvc/Configuration/Defaults/UseStaticFiles.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace Picums.Mvc.Configuration.Defaults
+{
+    public sealed class UseStaticFiles : IDefault
+    {
+        public void Apply(StartupConfigurations host, params object[] arguments)
+        {
+            host.ASP.Add((app, env) => app.UseStaticFiles());
+        }
+    }
+}
