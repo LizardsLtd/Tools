c53a66721a568db399900f8b96cd08409e622f28
Szymon Sasin <szymon@sasin.eu>
2017-03-30 19:49:28 +0100

    added authentication default

--- src/Picums.Mvc/Configuration/Defaults/AuthenticationDefault.cs
+++ src/Picums.Mvc/Configuration/Defaults/AuthenticationDefault.cs
@@ -3,23 +3,24 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
 using Picums.Data.Claims;
+using Picums.Mvc.Claims.Entities;
 
 namespace Picums.Mvc.Configuration.Defaults
 {
     public sealed class AuthenticationDefault<TUser, TUserStore> : BasicDefault
-            where TUser : class, IClaimsProvider, IUser
-            where TUserStore : class, IUserStore<TUser>
+        where TUser : class, IClaimsProvider, IUser
+        where TUserStore : class, IUserStore<TUser>
     {
         protected override void ConfigureApp(IApplicationBuilder app, IHostingEnvironment env)
         {
-            app.UseIdentity
+            app.UseIdentity();
         }
 
         protected override void ConfigureServices(IServiceCollection services)
         {
-            services
-                    .AddClaimsIdentity<TUser>()
-                    .AddScoped<IUserStore<TUser>, TUserStore>()
+            services.AddIdentity<TUser, string>();
+            services.AddScoped<IUserClaimsPrincipalFactory<TUser>, ClaimsPrincipalFactory<TUser>>();
+            services.AddScoped<IUserStore<TUser>, TUserStore>();
         }
     }
 }
