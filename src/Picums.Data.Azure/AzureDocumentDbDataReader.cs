8f1edcb6a84e0176f0cb8d137691eb8dc10466ef
Szymon Sasin <szymon@sasin.eu>
2017-04-13 21:46:40 +0100

    trying to debug azure query

--- src/Picums.Data.Azure/AzureDocumentDbDataReader.cs
+++ src/Picums.Data.Azure/AzureDocumentDbDataReader.cs
@@ -10,42 +10,42 @@ using Picums.Data.Domain;
 
 namespace Picums.Data.Azure
 {
-	internal sealed class AzureDocumentDbDataReader<T> : IDataReader<T>
-		where T : IAggregateRoot
-	{
-		private readonly DocumentClient client;
-		private readonly Uri collectionUri;
-		private readonly ILogger logger;
+    internal sealed class AzureDocumentDbDataReader<T> : IDataReader<T>
+        where T : IAggregateRoot
+    {
+        private readonly DocumentClient client;
+        private readonly Uri collectionUri;
+        private readonly ILogger logger;
 
-		public AzureDocumentDbDataReader(
-			DocumentClient client
-			, Uri collectionUri
-			, ILogger logger)
-		{
-			this.client = client;
-			this.collectionUri = collectionUri;
-			this.logger = logger;
-		}
+        public AzureDocumentDbDataReader(
+            DocumentClient client
+            , Uri collectionUri
+            , ILogger logger)
+        {
+            this.client = client;
+            this.collectionUri = collectionUri;
+            this.logger = logger;
+        }
 
-		public Task<IEnumerable<T>> All()
-		{
-			this.logger.LogInformation($"AzureDocumentDbDataReader for {typeof(T).Name} All function");
-			return Task.FromResult<IEnumerable<T>>(this.QueryDocumentDb().ToArray());
-		}
+        public Task<IEnumerable<T>> All()
+        {
+            this.logger.LogInformation($"AzureDocumentDbDataReader for {typeof(T).Name} All function");
+            return Task.FromResult<IEnumerable<T>>(this.QueryDocumentDb().ToArray());
+        }
 
-		public Task<TResult> QueryFor<TResult>(Expression<Func<IQueryable<T>, TResult>> predicate)
-		{
-			this.logger.LogInformation($"AzureDocumentDbDataReader for {typeof(T).Name} QueryFor function");
-			return Task.FromResult(predicate.Compile().Invoke(this.QueryDocumentDb()));
-		}
+        public Task<TResult> QueryFor<TResult>(Expression<Func<IQueryable<T>, TResult>> predicate)
+        {
+            this.logger.LogInformation($"AzureDocumentDbDataReader for {typeof(T).Name} QueryFor function");
+            return Task.FromResult(predicate.Compile().Invoke(this.QueryDocumentDb()));
+        }
 
-		public Task<IQueryable<T>> Where(Expression<Func<T, bool>> predicate)
-		{
-			this.logger.LogInformation($"AzureDocumentDbDataReader for {typeof(T).Name} Where function");
-			return Task.FromResult(this.QueryDocumentDb().Where(predicate.Compile()).AsQueryable());
-		}
+        public Task<IQueryable<T>> Where(Expression<Func<T, bool>> predicate)
+        {
+            this.logger.LogInformation($"AzureDocumentDbDataReader for {typeof(T).Name} Where function");
+            return Task.FromResult(this.QueryDocumentDb().Where(predicate.Compile()).AsQueryable());
+        }
 
-		private IOrderedQueryable<T> QueryDocumentDb()
-			=> client.CreateDocumentQuery<T>(collectionUri);
-	}
+        private IOrderedQueryable<T> QueryDocumentDb()
+            => client.CreateDocumentQuery<T>(collectionUri);
+    }
 }
