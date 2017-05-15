1beed819637498170b7ad8bd0fecd7f117007dfd
Szymon Sasin <szymon@sasin.eu>
2017-05-15 22:06:27 +0100

    Translations does not require to use DatabaseParts from DI container

--- src/Picums.Localisation/Data/DataTranslationProvider.cs
+++ src/Picums.Localisation/Data/DataTranslationProvider.cs
@@ -1,15 +1,19 @@
-﻿namespace Picums.Localisation.Data.Services
+﻿using System.Collections.Generic;
+using Picums.Data.CQRS;
+using Picums.Data.CQRS.DataAccess;
+
+namespace Picums.Localisation.Data.Services
 {
     public sealed class DataTranslationProvider : ITranslationSetProvider
     {
-        private readonly GetAllTranslationsQuery query;
+        private readonly IAsyncQuery<IEnumerable<TranslationItem>> query;
 
-        public DataTranslationProvider(GetAllTranslationsQuery query)
+        public DataTranslationProvider(GetAllTranslationsQuery query, DatabaseParts parts )
         {
-            this.query = query;
+            this.query = query.GetQuery(parts);
         }
 
         public TranslationSet GetTranslationSet()
-            => new TranslationSet(query.GetQuery().Execute().GetAwaiter().GetResult());
+            => new TranslationSet(query.Execute().Result);
     }
 }
--- src/Picums.Mvc/Configuration/Defaults/LocaliseUsingDatabaseDefault.cs
+++ src/Picums.Mvc/Configuration/Defaults/LocaliseUsingDatabaseDefault.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Microsoft.Extensions.DependencyInjection;
 using Picums.Data.CQRS.DataAccess;
 using Picums.Localisation.Data;
 using Picums.Localisation.Data.Services;
+using Microsoft.Extensions.Logging;
 
 namespace Picums.Mvc.Configuration.Defaults
 {
@@ -12,11 +14,19 @@ namespace Picums.Mvc.Configuration.Defaults
         public void Apply(StartupConfigurations host, IEnumerable<object> arguments)
         {
             host.Services.Add(services => services
-                    .AddTransient<GetAllTranslationsQuery>()
+                    //.AddTransient<GetAllTranslationsQuery>()
                     .AddTransient<AddNewTranslationCommandHandler>()
-                    .AddTransient<ITranslationSetProvider, DataTranslationProvider>());
+                    .AddTransient(serviceProvider => GetTranslationSetProvider(serviceProvider, GetParts(arguments))));
 
             host.Apply<CQRSDefaults>("Picums.Localisation");
         }
+
+        private ITranslationSetProvider GetTranslationSetProvider(IServiceProvider serviceProvider, DatabaseParts parts)
+            => new DataTranslationProvider(GetTranslationQuery(serviceProvider), parts);
+
+        private GetAllTranslationsQuery GetTranslationQuery(IServiceProvider serviceProvider)
+            => new GetAllTranslationsQuery(serviceProvider.GetService<IDataContext>(), serviceProvider.GetService<ILoggerFactory>());
+
+        private DatabaseParts GetParts(IEnumerable<object> arguments) => arguments.ElementAt(0) as DatabaseParts;
     }
 }
