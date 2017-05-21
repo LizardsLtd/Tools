efea94a21c5ba40afe98d7843aa89bf075b7db11
Szymon Sasin <szymon@sasin.eu>
2017-05-21 12:56:19 +0100

    trying to debug localistaion

--- build.ps1
+++
@@ -1,7 +0,0 @@
-Invoke-WebRequest -Uri https://raw.githubusercontent.com/dotnet/cli/master/scripts/obtain/dotnet-install.ps1 -OutFile dotnet-install.ps1
-
-./dotnet-install.ps1 -Version 2.0.0-preview1-005977 -Channel preview -InstallDir dt
-
-./dt/dotnet restore -v m
-./dt/dotnet build -v m
-./dt/dotnet pack -v m -c debug --include-symbols --include-source
