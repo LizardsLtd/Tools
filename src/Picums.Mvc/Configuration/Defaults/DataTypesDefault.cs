72d44dc4fcfd06c6c03dc8ecb4e8420511dd8051
Szymon Sasin <szymon@sasin.eu>
2017-06-03 14:10:02 +0100

    Added model binders for basic data types

---
+++ src/Picums.Mvc/Configuration/Defaults/DataTypesDefault.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Picums.Data.Types;
+using Picums.Mvc.ApplicationServices;
+
+namespace Picums.Mvc.Configuration.Defaults
+{
+    public sealed class DataTypesDefault : IDefault
+    {
+        public void Apply(StartupConfigurations host, IEnumerable<object> arguments)
+        {
+            host.MVC.Models
+                .AddModelBinderProvider<Address, AddressModelBinder>()
+                .AddModelBinderProvider<BankDetails, BankDetailsModelBinder>()
+                .AddModelBinderProvider<Email, EmailModelBinder>();
+        }
+    }
+}
