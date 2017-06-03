1035d37b1c164d42f635167684a61ab3ae67ca1b
Szymon Sasin <szymon@sasin.eu>
2017-06-04 00:32:37 +0100

    trying to make the binding to work properly

--- src/Picums.GeoCoding/SpatialJsonConverter.cs
+++ src/Picums.GeoCoding/SpatialJsonConverter.cs
@@ -1,26 +1,26 @@
-﻿using System;
-using Microsoft.Spatial;
-using Newtonsoft.Json;
+﻿//using System;
+//using Microsoft.Spatial;
+//using Newtonsoft.Json;
 
-namespace Picums.GeoCoding
-{
-    public sealed class SpatialJsonConverter : JsonConverter
-    {
-        public override bool CanConvert(Type objectType)
-            => typeof(GeographyPoint).Equals(objectType);
+//namespace Picums.GeoCoding
+//{
+//    public sealed class SpatialJsonConverter : JsonConverter
+//    {
+//        public override bool CanConvert(Type objectType)
+//            => typeof(GeographyPoint).Equals(objectType);
 
-        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
-        {
-            reader.Read();
-            var latitude = reader.ReadAsDecimal();
+//        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+//        {
+//            reader.Read();
+//            var latitude = reader.ReadAsDecimal();
 
-            reader.Read();
-            var longitude = reader.ReadAsDecimal();
+//            reader.Read();
+//            var longitude = reader.ReadAsDecimal();
 
-            return GeographyPoint.Create((double)latitude.Value, (double)longitude.Value);
-        }
+//            return GeographyPoint.Create((double)latitude.Value, (double)longitude.Value);
+//        }
 
-        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
-            => serializer.Serialize(writer, value);
-    }
-}
+//        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+//            => serializer.Serialize(writer, value);
+//    }
+//}
