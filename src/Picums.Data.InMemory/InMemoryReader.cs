71f827c9f26da502b6e7a20b881e8f756bd5f76c
Szymon Sasin <szymon@sasin.eu>
2017-06-08 19:58:27 +0100

    Added in memory implementation

--- src/Picums.Data.InMemory/InMemoryReader.cs
+++ src/Picums.Data.InMemory/InMemoryReader.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Picums.Data.CQRS.DataAccess;
 using Picums.Data.Domain;
@@ -17,30 +18,9 @@ namespace Picums.Data.InMemory
         }
 
         public Task<IEnumerable<T>> Collection(Func<T, bool> predicate)
-        {
-            throw new NotImplementedException();
-        }
+            => Task.FromResult(this.list.Where(predicate));
 
         public Task<Maybe<T>> Single(Func<T, bool> predicate, Func<IEnumerable<T>, T> reduce)
-        {
-            throw new NotImplementedException();
-        }
-
-        //public Task<IEnumerable<T>> All() => Task.FromResult(this.list.AsEnumerable());
-
-        //public Task<Maybe<T>> ById(Guid id)
-        //    => Task.FromResult<Maybe<T>>(this.list.FirstOrDefault(x => x.Id == id));
-
-        //public Task<Maybe<T>> FirstOrDefault(Expression<Func<T, bool>> predicate)
-        //    => Task.FromResult<Maybe<T>>(this.list.FirstOrDefault(predicate.Compile()));
-
-        //public Task<TResult> QueryFor<TResult>(Expression<Func<IQueryable<T>, TResult>> predicate)
-        //    => Task.FromResult(predicate.Compile().Invoke(this.list.AsQueryable()));
-
-        //public Task<Maybe<T>> SingleOrDefault(Expression<Func<T, bool>> predicate)
-        //    => Task.FromResult(this.list.SingleOrNothing(predicate.Compile()));
-
-        //public Task<IEnumerable<T>> Where(Expression<Func<T, bool>> predicate)
-        //    => Task.FromResult(this.list.Where(predicate.Compile()));
+            => Task.FromResult<Maybe<T>>(reduce(this.list.Where(predicate)));
     }
 }
