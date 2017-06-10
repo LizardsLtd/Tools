ca4de18a253d5af5a7bb882ae2bf3f634ec5dcb6
Szymon Sasin <szymon@sasin.eu>
2017-06-10 23:03:44 +0100

    reverted to having the constructor

--- src/Picums.Mvc/Configuration/Defaults/DependencyInjectionDefault.cs
+++ src/Picums.Mvc/Configuration/Defaults/DependencyInjectionDefault.cs
@@ -7,10 +7,14 @@ namespace Picums.Mvc.Configuration.Defaults
 {
     public sealed class DependencyInjectionDefault : BasicDefault
     {
+        private readonly List<Action<IServiceCollection>> configurationActions;
+
+        public DependencyInjectionDefault(params Action<IServiceCollection>[] configurationActions)
+        {
+            this.configurationActions = configurationActions.ToList();
+        }
+
         protected override void ConfigureServices(IServiceCollection services, IEnumerable<object> arguments)
-            => arguments
-                .Cast<Action<IServiceCollection>>()
-                .ToList()
-                .ForEach(x => x(services));
+            => configurationActions.ForEach(x => x(services));
     }
 }
