63b25562dd289ca2e694d97be01d030e24638cfe
Szymon Sasin <szymon@sasin.eu>
2017-06-15 22:54:54 +0100

    Added Search results tests

--- test/Picums.Tests.Search.Azure/AzureSearchService.fs
+++ test/Picums.Tests.Search.Azure/AzureSearchService.fs
@@ -1,11 +1,11 @@
-module Tests
+module Picums.Tests.Search.Azure
 
 open System
+open System.Linq
 open Xunit
 open FsUnit.Xunit
 open Microsoft.Spatial
 open Newtonsoft.Json
-open Picums.Mvc.Configuration
 open Microsoft.Extensions.Logging
 open FakeItEasy
 open Picums.Search.Azure
@@ -15,6 +15,23 @@ type TestSearchItem() =
         member this.Score = 0.0
 
 [<Fact>]
-let ``SearchResult can be empy`` () =
-    let result = new TestSearchItem()
+let ``SearchResults can be empy`` () =
+    let result = SearchResults.Empty
     result.HasResults |> should equal false
+
+[<Fact>]
+let ``SearchResults has sresult when provided with data`` () =
+    let result = new SearchResults<TestSearchItem>([new TestSearchItem(); new TestSearchItem()])
+    result.HasResults |> should equal true
+
+[<Fact>]
+let ``SearchResults saves results correctly`` () =
+    let result = new SearchResults<TestSearchItem>([new TestSearchItem(); new TestSearchItem()])
+    result.Results.Count() |> should equal 2
+
+[<Fact>]
+let ``Merge between two SearchResults works`` () =
+    let first = new SearchResults<TestSearchItem>([new TestSearchItem(); new TestSearchItem()])
+    let second = new SearchResults<TestSearchItem>([new TestSearchItem(); new TestSearchItem()])
+    let merged = first.Merge(second)
+    merged.Results.Count() |> should equal 4
