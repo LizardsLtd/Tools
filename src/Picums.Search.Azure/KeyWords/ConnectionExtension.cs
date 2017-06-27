c9e3040f6028b3eb41486267f98d0bff9595125b
Szymon Sasin <szymon@sasin.eu>
2017-06-27 22:47:34 +0100

    Add extension

---
+++ src/Picums.Search.Azure/KeyWords/ConnectionExtension.cs
@@ -0,0 +1,8 @@
+﻿namespace Picums.Search.Azure.KeyWords
+{
+    public static class ConnectionExtension
+    {
+        public static ISearchForParameter And(this ISearchForParameter first, ISearchForParameter second)
+            => new And(first, second);
+    }
+}
