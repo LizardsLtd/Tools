f67100b97aaa3b4860aed1dbc0ef359199875da6
Szymon Sasin <szymon@sasin.eu>
2017-07-03 21:32:32 +0100

    LocationKeyword has been updated to filter that works

--- src/Picums.Search.Azure/KeyWords/LocationKeyword.cs
+++ src/Picums.Search.Azure/KeyWords/LocationKeyword.cs
@@ -4,14 +4,18 @@
     {
         private readonly double Longitude;
         private readonly double Latitude;
+        private readonly double distanceInKilometers;
+        private readonly string locationField;
 
-        public LocationKeyword(double longitude, double latitude)
+        public LocationKeyword(double longitude, double latitude, string locationField, double distanceInKilometers = 25)
         {
             this.Longitude = longitude;
             this.Latitude = latitude;
+            this.locationField = locationField;
+            this.distanceInKilometers = distanceInKilometers;
         }
 
         public string GetSearchCommmand()
-            => $"geo.distance({this.Longitude}, {this.Latitude})";
+            => $"$filter=geo.distance({locationField}, geography'POINT({this.Longitude} {this.Latitude})') lt {distanceInKilometers}";
     }
 }
