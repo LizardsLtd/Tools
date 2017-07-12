15a4c9074a29595c65ccf51639f597866c668fcf
Szymon Sasin <szymon@sasin.eu>
2017-07-12 07:46:58 +0100

    added build script

---
+++ Scripts/build.ps1
@@ -0,0 +1,7 @@
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/dotnet/cli/master/scripts/obtain/dotnet-install.ps1 -OutFile dotnet-install.ps1
+
+./dotnet-install.ps1 -Version 2.0.0-preview1-005977 -Channel preview -InstallDir dt
+
+./dt/dotnet restore -v m
+./dt/dotnet build -v m --configuration debug
+./dt/dotnet pack -v m --configuration debug --include-symbols --include-source
