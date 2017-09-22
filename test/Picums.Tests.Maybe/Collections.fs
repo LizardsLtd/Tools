80d7e615bc1bfbea1ce127f51d6292bb89799c7d
Szymon Sasin <szymon@sasin.eu>
2017-09-23 00:56:40 +0100

    Finish converting maybe test into F#

---
+++ test/Picums.Tests.Maybe/Collections.fs
@@ -0,0 +1,48 @@
+﻿module Picums.Tests.Maybe.Collections
+
+    open System
+    open System.Linq
+    open Picums.Maybe
+    open Picums.Tests
+    open Xunit
+    open FsUnit.Xunit
+
+    let exampleCollection = [1; 2; 3; 4; 5]
+
+    [<Fact>]
+    let ``Cast To Maybe Collection Works`` () =
+        let collection = exampleCollection.ToMaybeList()
+        collection.All(fun x -> x.IsSome) |> should be True
+
+    [<Fact>]
+    let ``SingleOrNothionglet `` () =
+        let collection = [1;]
+        let result = collection.SingleOrNothing();
+        result.IsSome |> should be True
+
+    [<Fact>]
+    let ``SingleOrNothingWoithQuerylet `` () =
+        let result = exampleCollection.SingleOrNothing(fun x -> x = 1);
+        result.IsSome |> should be True
+
+    [<Fact>]
+    let ``FirstOrNothionglet `` () =
+        let collection = [1;]
+        let result = collection.FirstOrNothing();
+        result.IsSome |> should be True
+
+    [<Fact>]
+    let ``FirstOrNothingWoithQuerylet `` () =
+        let result = exampleCollection.FirstOrNothing(fun x -> x = 1);
+        result.IsSome |> should be True
+
+    [<Fact>]
+    let ``LastOrNothionglet `` ()=
+        let collection = [1;]
+        let result = collection.LastOrNothing();
+        result.IsSome |> should be True
+
+    [<Fact>]
+    let ``LastOrNothingWoithQuerylet `` () =
+        let result = exampleCollection.LastOrNothing(fun x -> x = 1);
+        result.IsSome |> should be True
