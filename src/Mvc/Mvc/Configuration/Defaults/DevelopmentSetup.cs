fdbe496b9b63e42870a6c8dc9c31181a9d6c2e3e
Szymon M Sasin <szymon@sasin.eu>
2017-10-09 21:36:37 +0100

    trying to solve the missing reference problem

--- src/Mvc/Mvc/Configuration/Defaults/DevelopmentSetup.cs
+++ src/Mvc/Mvc/Configuration/Defaults/DevelopmentSetup.cs
@@ -2,16 +2,17 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Picums.Mvc.Configuration.Defaults
 {
     public sealed class DevelopmentSetup : BasicDefault
     {
         protected override void ConfigureApp(
-            IApplicationBuilder app
-            , IHostingEnvironment env
-            , ILoggerFactory loggerFactory
-            , IEnumerable<object> arguments)
+            IApplicationBuilder app,
+            IHostingEnvironment env,
+            ILoggerFactory loggerFactory,
+            IEnumerable<object> arguments)
         {
             if (env.IsDevelopment())
             {
