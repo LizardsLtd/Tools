0b96b3a1ed3ce6e7959c741217e6a94aae6dbe05
Szymon Sasin <szymon@sasin.eu>
2017-10-10 23:37:14 +0100

    project is compiling

--- src/Picums.Tests/TranslationSet.cs
+++ src/Picums.Tests/TranslationSet.cs
@@ -1,39 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Globalization;
+//using System.Linq;
 
-namespace Picums.Mvc.Localisation
-{
-    public sealed class TranslationSet
-    {
-        private readonly HashSet<TranslationItem> translationData;
+//namespace Picums.Mvc.Localisation
+//{
+//    public sealed class TranslationSet
+//    {
+//        private readonly HashSet<TranslationItem> translationData;
 
-        public TranslationSet(IEnumerable<TranslationItem> items)
-        {
-            translationData = new HashSet<TranslationItem>(items, new CultureAndKeyComparer());
-        }
+//        public TranslationSet(IEnumerable<TranslationItem> items)
+//        {
+//            translationData = new HashSet<TranslationItem>(items, new CultureAndKeyComparer());
+//        }
 
-        public TranslationSet Merge(TranslationSet additionalData)
-            => new TranslationSet(translationData.Union(additionalData.translationData));
+//        public TranslationSet Merge(TranslationSet additionalData)
+//            => new TranslationSet(translationData.Union(additionalData.translationData));
 
-        public string GetTranslation(CultureInfo culture, string key)
-            => this.translationData
-                .Where(x => CompareWithCaseIgnored(x.CultureName, culture.TwoLetterISOLanguageName))
-                .Where(x => CompareWithCaseIgnored(x.TranslationKey, key))
-                .Select(x => x.Value)
-                .DefaultIfEmpty(key)
-                .FirstOrDefault();
+//        public string GetTranslation(CultureInfo culture, string key)
+//            => this.translationData
+//                .Where(x => CompareWithCaseIgnored(x.CultureName, culture.TwoLetterISOLanguageName))
+//                .Where(x => CompareWithCaseIgnored(x.TranslationKey, key))
+//                .Select(x => x.Value)
+//                .DefaultIfEmpty(key)
+//                .FirstOrDefault();
 
-        internal IEnumerable<(string, string)> GetAll(CultureInfo culture)
-            => this.translationData
-                .Where(x => CompareWithCaseIgnored(x.CultureName, culture.TwoLetterISOLanguageName))
-                .Select(x => (x.TranslationKey, x.Value));
+//        internal IEnumerable<(string, string)> GetAll(CultureInfo culture)
+//            => this.translationData
+//                .Where(x => CompareWithCaseIgnored(x.CultureName, culture.TwoLetterISOLanguageName))
+//                .Select(x => (x.TranslationKey, x.Value));
 
-        internal IEnumerable<(string, string)> GetAll(object currentCulture)
-            => this.GetAll(currentCulture as CultureInfo);
+//        internal IEnumerable<(string, string)> GetAll(object currentCulture)
+//            => this.GetAll(currentCulture as CultureInfo);
 
-        private bool CompareWithCaseIgnored(string first, string second)
-                    => string.Equals(first, second, StringComparison.OrdinalIgnoreCase);
-    }
-}
+//        private bool CompareWithCaseIgnored(string first, string second)
+//                    => string.Equals(first, second, StringComparison.OrdinalIgnoreCase);
+//    }
+//}
