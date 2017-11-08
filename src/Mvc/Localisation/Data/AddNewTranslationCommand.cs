090b96a5d3f4518f89a063b60f253dc0b08807ba
Szymon Sasin <szymon@sasin.eu>
2017-11-08 18:23:52 +0000

    removed database name

--- src/Mvc/Localisation/Data/AddNewTranslationCommand.cs
+++ src/Mvc/Localisation/Data/AddNewTranslationCommand.cs
@@ -1,20 +1,15 @@
-﻿using Picums.Data.CQRS;
-
-namespace Picums.Mvc.Localisation.DataStorage
+﻿namespace Picums.Mvc.Localisation.DataStorage
 {
     public sealed class AddNewTranslationCommand : CommandBase, ICommand
     {
         /// <summary>Record Constructor</summary>
-        /// <param name="translationItem"><see cref="TranslationItem"/></param>
+        /// <param name="translationItem"><see cref="TranslationItem"/></param
         /// <param name="databaseName"><see cref="DatabaseName"/></param>
-        public AddNewTranslationCommand(TranslationItem translationItem, string databaseName)
+        public AddNewTranslationCommand(TranslationItem translationItem)
         {
             this.TranslationItem = translationItem;
-            this.DatabaseName = databaseName;
         }
 
         public TranslationItem TranslationItem { get; }
-
-        public string DatabaseName { get; }
     }
 }
