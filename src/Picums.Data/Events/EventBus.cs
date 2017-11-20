4fa42bb7445530e0e17eda796e43ef1fef5469c6
Szymon Sasin <szymon@sasin.eu>
2017-11-20 22:20:03 +0000

    Added eventbus implementation

--- src/Picums.Data/Events/EventBus.cs
+++ src/Picums.Data/Events/EventBus.cs
@@ -1,19 +1,39 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Picums.Data.Events
 {
-
     public sealed class EventBus : IEventBus
     {
-        public Task Publish<T>(T @event) where T : IEvent
+        private readonly List<(Type, object)> subscribers;
+
+        public EventBus()
         {
-            throw new NotImplementedException();
+            this.subscribers = new List<(Type, object)>();
         }
 
-        public Task Subscribe<T>(Action<T> subscriber) where T : IEvent
+        public Task Publish<T>(T @event)
+            where T : IEvent
         {
-            throw new NotImplementedException();
+            var type = typeof(T);
+            this.subscribers
+                .Where(x => x.Item1 == type)
+                .Select(x => x.Item2)
+                .Cast<Action<T>>()
+                .ToList()
+                .ForEach(action => action(@event));
+
+            return Task.CompletedTask;
+        }
+
+        public Task Subscribe<T>(Action<T> subscriber)
+            where T : IEvent
+
+        {
+            this.subscribers.Add((typeof(T), subscriber));
+            return Task.CompletedTask;
         }
     }
 }
